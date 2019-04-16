980d22a1495b1fb8d6684c589a30f3bf03e5781b
fangbin <fangbin@duiba.com.cn>
2019-04-16 09:53:18 +0800

    update

--- bin/fb-xcode.js
+++ bin/fb-xcode.js
@@ -1,14 +1,14 @@
-const program = require('commander');
-const download = require('download-git-repo');
-const chalk = require('chalk');
-const ora = require('ora');
+var program = require('commander');
+var download = require('download-git-repo');
+var chalk = require('chalk');
+var ora = require('ora');
 program
   .version('1.0.0')
   .option('-i, init [name]', '初始化fb-xcode项目')
   .parse(process.argv);
 
 if (program.init) {
-  const spinner = ora('正在从github下载fb-xcode').start();
+  var spinner = ora('正在从github下载fb-xcode').start();
   download('fang-bin/xcode-cli', program.init, function (err) {
     if (!err) {
       console.info(chalk.blueBright('下载成功'));
